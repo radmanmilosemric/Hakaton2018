1e0df3aabf7764c2a2469f3b8407e5c725c39ea4
Milos Radman <milos.radman.emric@outlook.com>
2018-03-03 10:17:30 +0100

    ...

---
+++ NISHakaton2018/NisHakaton2018/DataModels/KatalogBS.cs
@@ -0,0 +1,13 @@
+﻿
+namespace NisHakaton2018.DataModels
+{
+    public class KatalogBS
+    {
+        public string SifraBS { get; set; }
+        public string Naziv { get; set; }
+        public string Brend { get; set; }
+        public string Adresa { get; set; }
+        public string Mesto { get; set; }
+        public string GpsLat { get; set; }
+        public string GpsLon { get; set; } }
+}
