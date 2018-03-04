cdd02c075726b85a3280f2d6f095ae2235bb5259
Nikola Milenkovic <nikola.milenkovic@outlook.com>
2018-03-04 11:40:21 +0100

    Lists

---
+++ NISHakaton2018/POSSystemNIS/Models/KupacProizvod.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+ 
+
+namespace POSSystemNIS.Models
+{
+    [Table("KupacProizvod")]
+    public class KupacProizvod
+    {
+        [Key]
+        public int ID { get; set; }
+        public string LoyaltyKartica { get; set; }
+        public string SifraRobe { get; set; }
+        public int BrojKupovina { get; set; }
+    }
+}
---
+++ NISHakaton2018/POSSystemNIS/Models/PairedItem.cs
@@ -0,0 +1,25 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace POSSystemNIS.Models
+{
+
+    [Table("PairedItems")]
+    public class PairedItem
+    {
+        [Key]
+        public int PairedItemId { get; set; }
+        public string TstSifraRobe { get; set; }
+        public string TstNazivRobe { get; set; }
+        public string TndSifraRobe { get; set; }
+        public string TndNazivRobe { get; set; }
+        public int RankNo { get; set; }
+        public int Count { get; set; }
+        public int PartOfDay { get; set; }
+        public string SifraBS { get; set; }
+        public bool IsWeekend { get; set; }
+
+    }
+}
