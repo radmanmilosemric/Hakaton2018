7e238312923892e13e50036ad692a0cf5cd516f5
Nikola Milenkovic <nikola.milenkovic@outlook.com>
2018-03-04 13:55:29 +0100

    Top6

---
+++ NISHakaton2018/POSSystemNIS/Models/PartOfDay.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSSystemNIS.Models
+{
+    public class PartOfDay
+    {
+        public int PartOfDayId { get; set; }
+        public string Description { get; set; }
+    }
+}
